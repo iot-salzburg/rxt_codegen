464770dc214bff52493303fc56d05d0126f7f60c
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2022-02-11 16:57:22 +0100

    added first previev version of OPCUA codegenerator; not yet finished

--- source/codegen_opcua_generator.py
+++ source/codegen_opcua_generator.py
@@ -29,7 +29,7 @@ class OPCUAGeneratorClass():
 
 		for blocks in self.listBlocks:
 			assetName = blocks[0].assetName.lower()
-			self.dump_self(filename + assetName + ".py", assetName, blocks)
+			self.dump_self(filename + "agent_rxta_" + assetName + "_sim_01.py", assetName, blocks)
 
 	#--------------------------------------------
 	# dump all blocks of one asset to file
@@ -39,10 +39,57 @@ class OPCUAGeneratorClass():
 		# imports and Co
 		self.c = codegen_generator_helper.GeneratorHelper()
 		self.c.begin(tab="    ")
-		self.c.write('TODO: Generate OPCUA')
+		self.c.write('import asyncio\n')
+		self.c.write('from logging import setLogRecordFactory\n')
+		self.c.write('import robXTask.rxtx_helpers as rxtx_helpers\n\n')
+		self.c.write('import rxta_ARTI_Sim as rxta_ARTI_Sim\n\n')
+
+		# main routine
+		self.c.write('async def startRobXTask():\n')
+		self.c.indent()
+		self.c.write('print("*** startRobXTask")\n')
+		self.c.write('# This is the main-code - place any startup things here as needed...\n\n')
+		self.c.dedent()	
 		
-		# TODO
-		
+		# ---------------------------------------------------------------------------------------
+		# TODO: the following lines are not yet finished, just preview
+
+		# async call 1
+		self.c.write('async def on_rxte__message__FetchDrink__rxtx_helpers(messages):\n')
+		self.c.indent()
+		self.c.write('async for message in messages:\n')
+		self.c.indent()
+		self.c.write('await rxtx_helpers.logMessageReceived(message)\n')
+		self.c.write('print("*** on_rxte__message__FetchWater__rxtx_helpers()")\n')
+		self.c.write('sDrink = str(message.payload.decode("utf-8")).strip()\n')
+		self.c.write('print("got Message: " + sDrink)\n')
+		self.c.write('await rxtx_helpers.log(rxtx_helpers.enLogType.BLOCKLY,"rxta_ARTI_Sim.MoveToLocationARTI(Goal_Roboter)")\n')
+		self.c.write('await rxta_ARTI_Sim.MoveToLocationARTI("Goal_Roboter")\n')
+		self.c.write('await rxtx_helpers.sendMessage("GrabDrink", sDrink)\n\n')
+		self.c.dedent()
+		self.c.dedent()
+
+		# async call 2
+		self.c.write('async def on_rxte__message__DrinkGrabbed__rxtx_helpers(messages):\n')
+		self.c.indent()
+		self.c.write('async for message in messages:\n')
+		self.c.indent()
+		self.c.write('await rxtx_helpers.logMessageReceived(message)\n')
+		self.c.write('print("*** on_rxte__message__DrinkGrabbed__rxtx_helpers()")\n')
+		self.c.write('sDrink = str(message.payload.decode("utf-8")).strip()\n')
+		self.c.write('print("got Message: " + sDrink)\n')
+		self.c.write('await rxtx_helpers.log(rxtx_helpers.enLogType.BLOCKLY,"rxta_ARTI_Sim.MoveToLocationARTI(Goal_Couch)")\n')
+		self.c.write('await rxta_ARTI_Sim.MoveToLocationARTI("Goal_Couch")\n')
+		self.c.write('await rxtx_helpers.sendMessage("DrinkFetched", sDrink)\n\n')
+		self.c.dedent()
+		self.c.dedent()
+
+		# start async
+		self.c.write('rxtx_helpers.startAsync()\n')
+
+		# TODO : end section
+		# ---------------------------------------------------------------------------------------
+
 		# write to filestream
 		os.makedirs(os.path.dirname(filename), exist_ok=True) # Note: only works in Python 3.6(!)
 		f = open(filename,'w')
